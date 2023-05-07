b1970f3b9473945a8fb4c7aa041bbabbe39b5460
UncknownGuy <130330950+UncknownGuy@users.noreply.github.com>
2023-05-07 13:15:54 -0700

    Haruka 17.9

--- command/help.js
+++ command/help.js
@@ -15,95 +15,95 @@ function toCommas(x) {
 exports.allMenu = (role, ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Idul Fitri 🌙 
+*Hello* {pushname !== undefined ? pushname : 'Kak'}
 ${mundur}
 
 *STATISTICS*
- • Upload : ${upload}
- • Downloads : ${download}
+|-- *Upload* : ${upload}
+|-- *Downloads* : ${download}
 
 *BOT INFO*
- • Creator : ${ownerName}
- • Bot Name : ${botName}
- • Time : ${jam}
- • Date : ${tanggal(new Date())}
- • Runtime : ${runtime(process.uptime())}
+|-- *Created By* : ${ownerName}
+|-- *Bot Name* : ${botName}
+|-- *Time* : ${jam}
+|-- *Date* : ${tanggal(new Date())}
+|-- *Uptime* : ${runtime(process.uptime())}
 
 *USER INFO*
- • Name : ${pushname !== undefined ? pushname : '-'}
- • Status : ${isCreator ? 'Owner' : isPremium ? 'Premium' : 'Free'}
- • Limit : ${isCreator ? 'Unlimited' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
- • Limit Game : ${isCreator ? 'Unlimited' : cekGLimit(sender, gcount, glimit)}
- • Balance : ${getBalance(sender, balance)}
- • Role : ${role}
+|-- *Name* : ${pushname !== undefined ? pushname : '-'}
+|-- *Status* : ${isCreator ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+|-- *Limit* : ${isCreator ? 'Unlimited' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+|-- *Limit Game* : ${isCreator ? 'Unlimited' : cekGLimit(sender, gcount, glimit)}
+|-- *Balance* : ${getBalance(sender, balance)}
+|-- *Role* : ${role}
  
  
 *MAIN MENU*${petik}
- • ${prefix}menu
- • ${prefix}infobot
- • ${prefix}donate
- • ${prefix}dashboard
- • ${prefix}owner
- • ${prefix}cekdrive
- • ${prefix}cekbandwidth
- • ${prefix}cekpremium
- • ${prefix}listpremium
- • ${prefix}listsewa
- • ${prefix}speed
- • ${prefix}runtime
- • ${prefix}listbahasa${petik}
+| • ${prefix}*menu*
+| • ${prefix}*infobot*
+| • ${prefix}*donate*
+| • ${prefix}*dashboard*
+| • ${prefix}*owner*
+| • ${prefix}*cekdrive*
+| • ${prefix}*cekbandwidth*
+| • ${prefix}*cekpremium*
+| • ${prefix}*listpremium*
+| • ${prefix}*listsewa*
+| • ${prefix}*speed*
+| • ${prefix}*runtime*
+| • ${prefix}*listbahasa*${petik}
 
 *CONVERTER/TOOLS*${petik} 
- • ${prefix}sticker
- • ${prefix}stickerwm
- • ${prefix}smeme
- • ${prefix}toimg
- • ${prefix}tovideo
- • ${prefix}tomp3
- • ${prefix}ttp
- • ${prefix}attp
- • ${prefix}emojimix
- • ${prefix}ai
- • ${prefix}aiimg
- • ${prefix}aidraw
- • ${prefix}chatgpt
- • ${prefix}nuliskiri
- • ${prefix}nuliskanan
- • ${prefix}foliokiri
- • ${prefix}foliokanan
- • ${prefix}say
- • ${prefix}translate${petik}
+| • ${prefix}sticker
+| • ${prefix}stickerwm
+| • ${prefix}smeme
+| • ${prefix}toimg
+| • ${prefix}tovideo
+| • ${prefix}tomp3
+| • ${prefix}ttp
+| • ${prefix}attp
+| • ${prefix}emojimix
+| • ${prefix}ai
+| • ${prefix}aiimg
+| • ${prefix}aidraw
+| • ${prefix}chatgpt
+| • ${prefix}nuliskiri
+| • ${prefix}nuliskanan
+| • ${prefix}foliokiri
+| • ${prefix}foliokanan
+| • ${prefix}say
+| • ${prefix}translate${petik}
 
 *ANONYMOUS CHATS*${petik}
- • ${prefix}menfess
- • ${prefix}confess
- • ${prefix}balasmenfess
- • ${prefix}tolakmenfess
- • ${prefix}stopmenfess
- • ${prefix}anonymous
- • ${prefix}start
- • ${prefix}next
- • ${prefix}stop
- • ${prefix}sendprofile${petik}
+| • ${prefix}menfess
+| • ${prefix}confess
+| • ${prefix}balasmenfess
+| • ${prefix}tolakmenfess
+| • ${prefix}stopmenfess
+| • ${prefix}anonymous
+| • ${prefix}start
+| • ${prefix}next
+| • ${prefix}stop
+| • ${prefix}sendprofile${petik}
 
 *STORE MENU*${petik}
- • ${prefix}list
- • ${prefix}addlist
- • ${prefix}dellist
- • ${prefix}update
- • ${prefix}jeda
- • ${prefix}tambah
- • ${prefix}kurang
- • ${prefix}kali
- • ${prefix}bagi
- • ${prefix}delsetdone
- • ${prefix}changedone
- • ${prefix}setdone
- • ${prefix}delsetproses
- • ${prefix}changeproses
- • ${prefix}setproses
- • proses < reply chat >
- • done < reply chat >${petik}
+| • ${prefix}list
+| • ${prefix}addlist
+| • ${prefix}dellist
+| • ${prefix}update
+| • ${prefix}jeda
+| • ${prefix}tambah
+| • ${prefix}kurang
+| • ${prefix}kali
+| • ${prefix}bagi
+| • ${prefix}delsetdone
+| • ${prefix}changedone
+| • ${prefix}setdone
+| • ${prefix}delsetproses
+| • ${prefix}changeproses
+| • ${prefix}setproses
+| • proses < reply chat >
+| • done < reply chat >${petik}
 
 *RPG GAMES*${petik}
  • ${prefix}inventory
@@ -151,41 +151,41 @@ ${mundur}
  • ${prefix}gitclone${petik}
 
 *GROUP MENU*${petik}
- • ${prefix}autoaigc
- • ${prefix}afk
- • ${prefix}welcome
- • ${prefix}left
- • ${prefix}pppanjanggc
- • ${prefix}setopen
- • ${prefix}changesetopen
- • ${prefix}delsetopen
- • ${prefix}setclose
- • ${prefix}changesetclose
- • ${prefix}delsetclose
- • ${prefix}setwelcome
- • ${prefix}changewelcome
- • ${prefix}delsetwelcome
- • ${prefix}setleft
- • ${prefix}changeleft
- • ${prefix}delsetleft
- • ${prefix}linkgc
- • ${prefix}setppgc
- • ${prefix}setppgc2 
- • ${prefix}setnamegc
- • ${prefix}setdesc
- • ${prefix}antilink
- • ${prefix}antiwame
- • ${prefix}antilink2
- • ${prefix}antiwame2
- • ${prefix}open
- • ${prefix}close
- • ${prefix}add
- • ${prefix}kick
- • ${prefix}promote
- • ${prefix}demote
- • ${prefix}revoke
- • ${prefix}hidetag
- • ${prefix}checksewa${petik}
+| • ${prefix}autoaigc
+| • ${prefix}afk
+| • ${prefix}welcome
+| • ${prefix}left
+| • ${prefix}pppanjanggc
+| • ${prefix}setopen
+| • ${prefix}changesetopen
+| • ${prefix}delsetopen
+| • ${prefix}setclose
+| • ${prefix}changesetclose
+| • ${prefix}delsetclose
+| • ${prefix}setwelcome
+| • ${prefix}changewelcome
+| • ${prefix}delsetwelcome
+| • ${prefix}setleft
+| • ${prefix}changeleft
+| • ${prefix}delsetleft
+| • ${prefix}linkgc
+| • ${prefix}setppgc
+| • ${prefix}setppgc2 
+| • ${prefix}setnamegc
+| • ${prefix}setdesc
+| • ${prefix}antilink
+| • ${prefix}antiwame
+| • ${prefix}antilink2
+| • ${prefix}antiwame2
+| • ${prefix}open
+| • ${prefix}close
+| • ${prefix}add
+| • ${prefix}kick
+| • ${prefix}promote
+| • ${prefix}demote
+| • ${prefix}revoke
+| • ${prefix}hidetag
+| • ${prefix}checksewa${petik}
 
 *GAME MENU*${petik}
  • ${prefix}tebakgambar
@@ -204,15 +204,15 @@ ${mundur}
  • ${prefix}delcasino${petik}
 
 *SEARCH MENU*${petik}
- • ${prefix}lirik
- • ${prefix}grupwa
- • ${prefix}pinterest
- • ${prefix}ytsearch${petik}
+| • ${prefix}lirik
+| • ${prefix}grupwa
+| • ${prefix}pinterest
+| • ${prefix}ytsearch${petik}
 
 *RANDOM MENU*${petik}
- • ${prefix}cecan
- • ${prefix}cogan
- • ${prefix}waifu${petik}
+| • ${prefix}cecan
+| • ${prefix}cogan
+| • ${prefix}waifu${petik}
 
 
 *BALANCE MENU*${petik}
@@ -244,30 +244,30 @@ ${mundur}
  • ${prefix}delcmd${petik}
 
 *OWNERS MENU*${petik}
- • ${prefix}autoaipc
- • ${prefix}autoread
- • ${prefix}autobio
- • ${prefix}antidelete
- • ${prefix}antiviewonce
- • ${prefix}autorespond
- • ${prefix}anticall
- • ${prefix}autoblok212
- • ${prefix}join
- • ${prefix}left
- • ${prefix}self
- • ${prefix}public
- • ${prefix}setppbot2
- • ${prefix}setppbot
- • ${prefix}broadcast
- • ${prefix}bcimg
- • ${prefix}bcstik
- • ${prefix}bcvn
- • ${prefix}bcvideo
- • ${prefix}bcsewa
- • ${prefix}addpremium
- • ${prefix}delpremium
- • ${prefix}addsewa
- • ${prefix}delsewa${petik}
+| • ${prefix}autoaipc
+| • ${prefix}autoread
+| • ${prefix}autobio
+| • ${prefix}antidelete
+| • ${prefix}antiviewonce
+| • ${prefix}autorespond
+| • ${prefix}anticall
+| • ${prefix}autoblok212
+| • ${prefix}join
+| • ${prefix}left
+| • ${prefix}self
+| • ${prefix}public
+| • ${prefix}setppbot2
+| • ${prefix}setppbot
+| • ${prefix}broadcast
+| • ${prefix}bcimg
+| • ${prefix}bcstik
+| • ${prefix}bcvn
+| • ${prefix}bcvideo
+| • ${prefix}bcsewa
+| • ${prefix}addpremium
+| • ${prefix}delpremium
+| • ${prefix}addsewa
+| • ${prefix}delsewa${petik}
 
 *ASUPAN MENU*${petik}
  • ${prefix}chika
